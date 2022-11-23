334d574503797cf27083ea253e6bcbc59fc41147
ssollychoi <ssolly.choi@npage.co.kr>
2022-11-24 00:00:39 +0900

    Initial commit

---
+++ src/main/java/com/study/gusa/Study94Application.java
@@ -0,0 +1,13 @@
+package com.study.gusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study94Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study94Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/gusa/Study94ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.gusa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study94ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
